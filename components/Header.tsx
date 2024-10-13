0b54c1576efa3ed3acc6c7cc12bff24448261131
ezenjun <ezenjun07@gmail.com>
2024-10-13 16:14:00 +0900

    feat : header component 생성

---
+++ components/Header.tsx
@@ -0,0 +1,33 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { auth } from '@/lib/auth';
+
+export default async function Header() {
+  const session = await auth();
+
+  return (
+    <header className='flex justify-between items-center p-4 bg-white shadow-md'>
+      <div className='logo'>
+        <Link href='/home' className='text-xl font-bold'>
+          나만의 레시피
+        </Link>
+      </div>
+      <div className='actions space-x-2'>
+        {session?.user ? (
+          <>
+            <Button asChild variant='outline'>
+              <Link href='/recipe/create'>레시피 추가</Link>
+            </Button>
+            <Button>
+              <Link href='/api/auth/signout'>로그아웃</Link>
+            </Button>
+          </>
+        ) : (
+          <Button asChild>
+            <Link href='/api/auth/signin'>로그인</Link>
+          </Button>
+        )}
+      </div>
+    </header>
+  );
+}
