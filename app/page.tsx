e14bec3e2f5448f98f9720724cda1e8162325d15
ezenjun <ezenjun07@gmail.com>
2024-10-15 21:57:15 +0900

    design : 홈페이지 로그인 버튼 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import RecipeList from '@/components/RecipeList';
-import { Suspense } from 'react';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
 import { auth } from '@/lib/auth';
 
 export default async function HomePage() {
   const session = await auth();
   if (!session || !session.user?.email) {
     return (
-      <div className='flex flex-col h-full justify-center items-center p-4 gap-3 border-2 text-2xl'>
+      <div className='flex flex-col h-full justify-center items-center p-4 gap-3 border-2 text-2xl font-semibold'>
         레시피를 확인하려면 로그인해주세요
+        <Button>
+          <Link href='/api/auth/signin'>로그인하기</Link>
+        </Button>
       </div>
     );
   }
@@ -15,9 +19,7 @@ export default async function HomePage() {
   return (
     <div className='flex h-full justify-center overflow-y-hidden'>
       <div className='p-4 w-2/3 overflow-y-scroll no-scrollbar'>
-        <Suspense fallback={<div>Loading...</div>}>
-          <RecipeList userId={session.user.email} />
-        </Suspense>
+        <RecipeList userId={session.user.email} />
       </div>
     </div>
   );
